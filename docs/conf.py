3295ca1edd687852f04b83331674718e260ef48d
WlakerDevin12 <walkerdevin176@gmail.com>
2025-06-17 16:24:08 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,10 +2,9 @@ project = 'PeacocktvGuide12'
 author = 'Your Name'
 release = '1.0'
 
-extensions = ['recommonmark']
+extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-
